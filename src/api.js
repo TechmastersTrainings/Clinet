6b97166b247ad40a0a7d3e96e16757e376e026c9
airfoxer <airfoxersmps@gmail.com>
2026-06-12 12:18:33 +0530

    cleaned production api configuration

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 const API_BASE_URL =
-    import.meta.env.MODE === "development"
-        ? "http://localhost:5000/api"
-        : "https://api.techmaster.space/api" ||
-        "https://techmasters-innovations-drive.onrender.com/api";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"
+    : "https://api.techmaster.space/api";
 
 export default API_BASE_URL;
